93ecce89365906504b5bb1fd8a5886b8071e0d86
Rogue Panda <RoguePanda@Rogues-MacBook.local>
2013-10-10 21:48:49 -0700

    Initial Commit

---
+++ TimerV2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TimerV2
+//
+//  Created by Don Zhang on 13-10-10.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TimerV2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimerV2
+//
+//  Created by Don Zhang on 13-10-10.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
